d7293be6a14c57cba7e6e5d7cd4b0cebab15ecba
Jeremy Whitaker & jennifermccarthy <student@epicodus.com>
2014-07-22 16:33:40 -0700

    Looping through allergies and returning results. Tests pass!

--- spec/allergyScore_spec.rb
+++ spec/allergyScore_spec.rb
@@ -5,7 +5,9 @@ describe("allergyScore") do
   it("will take a user allergy score and return the allergen") do
     allergyScore(1).should(eq("You are allergic to eggs"))
   end
-
+  it("when the score is greater than an allergyScore the remainder will be applied to smaller allergens") do
+    allergyScore(3).should(eq("You are allergic to peanuts, eggs"))
+  end
 end
 
 
