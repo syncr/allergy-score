af3856948735953c94dc4fc3e9dc66b3da08bcf9
Jeremy Whitaker <student@epicodus.com>
2014-07-23 09:47:20 -0700

    Cleans up some errant comments and code.

--- lib/allergyScore.rb
+++ lib/allergyScore.rb
@@ -6,47 +6,9 @@ def allergyScore(input)
   allergen_objects.each do |allergen, score|
     if input/score == 1
         allergy += (allergen+", ")
-        # puts allergen
         input=input%score
-    else
-        input=input%score
-
-
-     # else allergen_objects.next
-
-
-
-   # input%score == 0 do
-   #  puts allergen
-     #  "You are allergic to #{allergen}"
     end
   end
 
   "You are allergic to " + allergy.chop.chop
 end
-
-# allergyScore(1)
-
-# def allergyScore(input)
-#     allergens = ["cats","pollen","chocolate","tomatoes","strawberries","shellfish","peanuts","eggs"]
-#     scores = [128, 64, 32, 16, 8, 4, 2, 1]
-#     results= []
-
-#     scores.each do |score|
-#       if input/score == 1
-#       results[score]=(input/score).floor
-#       input=input%score
-#     end
-#     puts results
-#   end
-
-
-#   coinValues.forEach(function(coinValue){
-  #   numberOfEachCoin[i] = Math.floor(change/coinValues[i]);
-  #   change = change%coinValues[i];
-  #   userChange[i] =+ numberOfEachCoin[i] +" "+ coinage[i];
-  #   i++;
-  # });
-
-    # allergyEach=(input/score).floor
-    #   if allergyEach=score.key
